d801b84c395a99777099e9fe0017e1fe2db9fbda
Mikey <mikey@localhost.localdomain>
2019-03-19 15:21:03 +0100

    Implement low-level fs functions to read & write properties

--- docfiler/fs.scm
+++ docfiler/fs.scm
@@ -2,14 +2,98 @@
 
 ;; This module contains the classes and functions to maintain a docfiler
 ;; filesystem database structure.
+;;
+;; The structure of the filesystem is as follows:
+;;
+;;   /base/
+;;     \_ 2019/
+;;        \_03/
+;;          \_20/
+;;            \_foo/
+;;              \_meta.gpg
+;;               _attachments/
+;;                \_attachment1.png.gpg
+;;                 _attachment2.pdf.gpg
+;;            \_bar/
+;;              \_meta.gpg
+;;               _attachments/
+;;                \_...
+;;
 
 ;; Code:
 
 (define-module (docfiler fs)
   #:use-module (oop goops)
-  #:export (<doc-fs> get-fs-base-path))
+  #:use-module (ice-9 ports)
+  #:use-module (ice-9 regex)
+  #:export (<doc-fs>
+            join-paths
+            fs-make-abs-path
+            fs-write-abs-path
+            fs-read-abs-path
+            fs-set-prop
+            fs-get-prop
+            fs-save-props
+            fs-load-props))
 
-(define-class <doc-fs> ()
-  (base-path #:getter get-fs-base-path))
+(define-class <doc-fs> () base-path)
 
+;; Exported functions
 
+(define-method (fs-read-abs-path (fs <doc-fs>)
+                                 (abs-path <string>))
+  ;; TODO: Possibly decrypt before reading here.
+  (if (file-exists? abs-path)
+      (let* ((in-port (open-input-file abs-path))
+             (obj (read in-port)))
+        (close-port in-port)
+        obj)
+      '()))
+
+(define-method (fs-write-abs-path (fs <doc-fs>)
+                                  (abs-path <string>)
+                                  (obj <list>))
+  ;; TODO: Possibly encrypt before writing here.
+  (let ((out-port (open-output-file abs-path)))
+    (write obj out-port)
+    (close-port out-port)))
+
+(define (join-paths . paths)
+  (regexp-substitute/global #f "/+" (string-join paths "/") 'pre "/" 'post))
+
+(define-method (fs-make-abs-path (fs <doc-fs>)
+                                 (path <string>))
+  (unless (file-exists? (slot-ref fs 'base-path))
+    (mkdir (slot-ref fs 'base-path)))
+  (let ((abs-path (join-paths (slot-ref fs 'base-path) path)))
+    (unless (file-exists? abs-path)
+      (mkdir abs-path))
+    abs-path))
+
+(define meta-filename "meta")
+
+(define-method (fs-load-props (fs <doc-fs>)
+                              (path <string>))
+  (fs-read-abs-path fs (join-paths (fs-make-abs-path fs path) meta-filename)))
+
+(define-method (fs-save-props (fs <doc-fs>)
+                              (path <string>)
+                              (props <list>))
+  (fs-write-abs-path fs
+                     (join-paths (fs-make-abs-path fs path) meta-filename)
+                     props))
+
+(define-method (fs-get-prop (fs <doc-fs>)
+                            (path <string>)
+                            (prop-name <string>))
+  (let ((props (fs-load-props fs path)))
+    (cdr (assoc prop-name props))))
+
+(define-method (fs-set-prop (fs <doc-fs>)
+                            (path <string>)
+                            (prop-name <string>)
+                            (prop-value <string>))
+  (let ((props (fs-load-props fs path)))
+    (fs-save-props fs path (assoc-set! props prop-name prop-value))))
+
+;; fs.scm end
--- test/fs_test.scm
+++ test/fs_test.scm
@@ -1,13 +1,64 @@
 (use-modules (docfiler fs)
              (docfiler tap)
-             (oop goops))
+             (oop goops)
+             (ice-9 ftw))
 
-(define t (make <tap-test-collection>))
+(define test (make <tap-test-collection>))
 
-(let ((fs-base-path "/tmp/test_doc_filer_base")
-      (fs (make <doc-fs>)))
-  (slot-set! fs 'base-path fs-base-path)
-  (register-test t "the base-path setter works"
-                 (is-eq (slot-ref fs 'base-path) fs-base-path)))
+(register-test
+ test "ensure path joining works"
+ (is-eq "a/b/c/d" (join-paths "a////" "b" "//c////" "d")))
 
-(run-tests t)
+(define fs (make <doc-fs>))
+(define tmp-base
+  (join-paths "/tmp"
+              (string-append "test_" (number->string (random 1000000000)))))
+(slot-set! fs 'base-path tmp-base)
+
+(register-test
+ test "abs path creation works"
+ (is-eq (join-paths tmp-base "baz") (fs-make-abs-path fs "baz")))
+
+(define test-meta-path (join-paths (fs-make-abs-path fs "foobar") "test_meta"))
+(register-test
+ test "we can write out an object"
+ (is-ok (fs-write-abs-path fs test-meta-path '(1 2 3 4))))
+
+(register-test
+ test "we can read back our written object"
+ (is-eq '(1 2 3 4) (fs-read-abs-path fs test-meta-path)))
+
+(register-test
+ test "we can set properties"
+ (is-ok (fs-set-prop fs "foobar" "first-name" "John")))
+
+(register-test
+ test "we can set properties"
+ (is-ok (fs-set-prop fs "foobar" "last-name" "Smith")))
+
+(register-test
+ test "we can get a property back"
+ (is-eq "John" (fs-get-prop fs "foobar" "first-name")))
+
+(register-test
+ test "we can get a property back"
+ (is-eq "Smith" (fs-get-prop fs "foobar" "last-name")))
+
+(register-test
+ test "we can override a property"
+ (is-ok (fs-set-prop fs "foobar" "first-name" "Mike")))
+
+(register-test
+ test "we can get the overridden property back"
+ (is-eq "Mike" (fs-get-prop fs "foobar" "first-name")))
+
+(run-tests test)
+
+;; Cleanup the temporary files and directories.
+(nftw tmp-base
+      (lambda (filename statinfo flag base level)
+        (case flag
+          ((directory-processed) (rmdir filename))
+          ((regular) (delete-file filename)))
+        #t)
+      'depth)
